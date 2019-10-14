4a2fd9c7d9e4c38ec56632d185255d3dcff9a18c
Dauren <37897996+dauren2089@users.noreply.github.com>
2019-10-14 22:06:15 +0600

    Add files via upload
    4 task

---
+++ who-are-you.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#! curl https://api.github.com/users/dauren2089 | jq '.name'
+
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[52] | .name'
+
+
