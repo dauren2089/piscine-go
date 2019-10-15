b1c66eb9313e458f055abddaf36d91f161136304
dauren2089 <dauren.muk7@gmail.com>
2019-10-15 19:14:25 +0600

    who-are-you added without -sed

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ' .[] | select( .id == 70 ) | .name' | sed 's/"//g'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ' .[] | select( .id == 70 ) | .name'
