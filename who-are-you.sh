8f865ee9ee5d605b093ba0906e296064bfd93cf2
Dauren <37897996+dauren2089@users.noreply.github.com>
2019-10-15 11:16:24 +0600

    updating file
    who-are-you

--- who-are-you.sh
+++ who-are-you.sh
@@ -2,6 +2,6 @@
 
 #! curl https://api.github.com/users/dauren2089 | jq '.name'
 
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[52] | .name'
+#! curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[52] | .name'
 
-
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ' .[] | select( .id == 70 ) | .name'
