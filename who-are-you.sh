f8b9abec043171b78aa8a7e18c74b1ea9faa0b5b
Dauren <37897996+dauren2089@users.noreply.github.com>
2019-10-15 12:18:38 +0600

    changed v2
    have added permissions

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,7 +1,1 @@
-#! /bin/bash
-
-#! curl https://api.github.com/users/dauren2089 | jq '.name'
-
-#! curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[52] | .name'
-
 curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ' .[] | select( .id == 70 ) | .name'
