dcce04aaf53969b7c450afab25cdd6fdde6b1c55
dauren2089 <dauren.muk7@gmail.com>
2019-10-16 22:09:57 +0600

    printcomb2 has added

--- printcomb2.go
+++ printcomb2.go
@@ -7,7 +7,7 @@ func PrintComb2() {
 	for a := '0'; a < '9'; a++ {
 		for b := '0'; b < '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
-				for d := '1'; c <= '9'; d++ {
+				for d := '1'; d <= '9'; d++ {
 					//if (a < c || (a <= c && b <= d)) && !(a == c && b == d) {
 					if a <= c && b < d {
 						z01.PrintRune(a)
