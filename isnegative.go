09d121807f154d2a3c02984a16fc85ce086d6c0c
dauren2089 <dauren.muk7@gmail.com>
2019-10-16 18:17:22 +0600

    isnegative has added

---
+++ isnegative.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func isNegative(aRune int) {
+	if aRune < 0 {
+
+		z01.PrintRune('T')
+
+	} else {
+
+		z01.PrintRune('F')
+
+	}
+
+	z01.PrintRune(10)
+}
