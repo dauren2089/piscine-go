477d8556e5a2617d86c4411341c2f94b17ee1246
dauren2089 <dauren.muk7@gmail.com>
2019-10-16 22:27:24 +0600

    printcomb2 has added

--- printcomb2.go
+++ printcomb2.go
@@ -13,9 +13,11 @@ func PrintComb2() {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
+
 						z01.PrintRune(c)
 						z01.PrintRune(d)
-						if !(a == 9 && b == 8 && c == 9 && d == 9) {
+
+						if !(a == 9 && b == 8 && c == 9 && d == 8) {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
