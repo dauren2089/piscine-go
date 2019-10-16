5759ee3d02f303eaea76bf3bc3abd4ce9d654cc6
dauren2089 <dauren.muk7@gmail.com>
2019-10-16 21:07:12 +0600

    printrune has added

---
+++ printcomb.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+
+	for a := '0'; a <= '7'; a++ {
+		for b := '1'; b <= '8'; b++ {
+			for c := '2'; c <= '9'; c++ {
+
+				z01.PrintRune('c')
+				z01.PrintRune('b')
+				z01.PrintRune('c')
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+
+			}
+		}
+	}
+}
