af1a06a5b8b66f693884d176c51510b79deb1b7f
dauren2089 <dauren.muk7@gmail.com>
2019-10-16 21:19:58 +0600

    printrune has added

--- printcomb.go
+++ printcomb.go
@@ -7,13 +7,14 @@ func PrintComb() {
 	for a := '0'; a <= '7'; a++ {
 		for b := '1'; b <= '8'; b++ {
 			for c := '2'; c <= '9'; c++ {
-
-				z01.PrintRune(c)
-				z01.PrintRune(b)
-				z01.PrintRune(c)
-				if !(a == '7' && b == '8' && c == '9') {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+				if b != a && b > a {
+					z01.PrintRune(c)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
+					if !(a == '7' && b == '8' && c == '9') {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 				}
 			}
 		}
