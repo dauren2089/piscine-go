c066743ce36fa26a70d0a00bd264a27baa40d70e
dauren2089 <dauren.muk7@gmail.com>
2019-10-16 21:29:10 +0600

    printrune has added

--- printcomb.go
+++ printcomb.go
@@ -4,11 +4,11 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 
-	for a := '0'; a <= '7'; a++ {
-		for b := '1'; b <= '8'; b++ {
+	for a := '0'; a < '8'; a++ {
+		for b := '1'; b < '9'; b++ {
 			for c := '2'; c <= '9'; c++ {
 				if a < b && b < c {
-					z01.PrintRune(c)
+					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(c)
 					z01.PrintRune(' ')
