abe8fbc10f4b75e1b4b82ded3fb6037551c97715
dauren2089 <dauren.muk7@gmail.com>
2019-10-17 19:54:00 +0600

    have changed

--- printstr.go
+++ printstr.go
@@ -5,8 +5,9 @@ import "github.com/01-edu/z01"
 func PrintStr(str string) {
 
 	slice := []rune(str)
+
 	for _, word := range slice {
 		z01.PrintRune(word)
 	}
-	z01.PrintRune(10)
+	//z01.PrintRune(10)
 }
