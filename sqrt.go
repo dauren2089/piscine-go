4cd9507248b22a2e7bcb8fb926e2312b0ba90b11
dauren2089 <dauren.muk@gmail.com>
2019-10-21 18:30:24 +0600

    isprime has added

--- sqrt.go
+++ sqrt.go
@@ -1,11 +1,10 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb == 0 {
-		return 0
-	} else if nb < 0 {
+	if nb <= 0 {
 		return 0
 	}
+
 	for i := 0; i <= nb; i++ {
 		if i*i > nb {
 			return 0
