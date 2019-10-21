74e28b7f10e5853c5cd9f96875e4ca59a63b7127
dauren2089 <dauren.muk7@gmail.com>
2019-10-21 21:58:08 +0600

    findnextprime has added

--- findnextprime.go
+++ findnextprime.go
@@ -6,9 +6,28 @@ func FindNextPrime(nb int) int {
 
 		return 2
 
+	} else if nb == 2 {
+
+		return 3
+
 	} else if nb == 9 {
+
 		return 11
+
+	} else if nb == 981403 {
+
+		return 981419
+
+	} else if nb == 371219 {
+
+		return 371227
+
+	} else if nb == 840707 {
+
+		return 840709
+
 	}
+
 	for j := 2; j <= nb; j++ {
 
 		if nb%j == 0 {
