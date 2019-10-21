61f2c8e37278dee9a7065dbcb687a23d873862a6
dauren2089 <dauren.muk7@gmail.com>
2019-10-21 21:03:51 +0600

    isprime has added

--- isprime.go
+++ isprime.go
@@ -8,10 +8,6 @@ func IsPrime(nb int) bool {
 
 	} else if nb == 2 {
 		return true
-	} else if nb == 9 {
-		return false
-	} else if nb == 356587 {
-		return false
 	}
 
 	//for i := 1; i <= nb; i++ {
