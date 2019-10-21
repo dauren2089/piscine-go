34b135e210d365871d42cd6d4c24748103156004
dauren2089 <dauren.muk7@gmail.com>
2019-10-21 20:18:01 +0600

    recursivepower has added

--- recursivepower.go
+++ recursivepower.go
@@ -2,13 +2,21 @@ package piscine
 
 func RecursivePower(nb int, power int) int {
 
-	if nb > 0 {
+	if nb != 0 && power > 0 {
+
 		if power > 1 {
+
 			return nb * RecursivePower(nb, power-1)
+
 		}
+
 		return nb
-	} else if nb == 1{
+
+	} else if nb == 1 {
+
 		return 1
+
 	}
+
 	return 0
 }
