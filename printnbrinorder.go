2ebc0c86e3b324aaefb3cf36ac9722f1c4f27813
dauren2089 <dauren.muk@gmail.com>
2019-10-22 16:12:05 +0600

    printnbrinorder has changed

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -2,19 +2,60 @@ package piscine
 
 import (
 	"github.com/01-edu/z01"
-	"strconv"
 )
 
-func PrintNbrInOrder(n int) {
-	sentence := strconv.Itoa(n)
+func PrintNbrInOrder(n int64) //int64 {
+	if n < 0 {
+			return
+	}
+	if n == 0 {
 
-	for i := 48; i <= 57; i++ {
+		z01.PrintRune('0')
 
-		for _, value := range sentence {
-			if value == rune(i) {
-				z01.PrintRune(value)
+	}
+
+	var answer []int64
+
+	answer = IntToSlice(n, answer)
+
+	SortInteger(answer)
+
+	for i := range answer {
+
+		for j := '0'; j <= '9'; j++ {
+
+			if rune(answer[i] + 48) == j {
+
+				z01.PrintRune(j)
+
+			}
+
+
+		}
+
+	}
+
+	}
+
+func IntToSlice(n int64, sequence []int64) []int64 {
+	if n != 0 {
+		i := n % 10
+		// sequence = append(sequence, i) // reverse order output
+		sequence = append([]int64{i}, sequence...)
+		return IntToSlice(n/10, sequence)
+	}
+	return sequence
+}
+
+func SortInteger(table []int64) {
+	L := len(table)
+
+	for i := 0; i < L; i++ {
+
+		for j := 0; j < (L - 1 - i); j++ {
+			if table[j] > table[j+1] {
+				table[j], table[j+1] = table[j+1], table[j]
 			}
 		}
 	}
-
 }
