fb693e202fd1306d6ac941a27de9efbf0d8e20e8
dauren2089 <dauren.muk7@gmail.com>
2019-10-22 18:47:19 +0600

    nrune has changed

--- nrune.go
+++ nrune.go
@@ -7,14 +7,3 @@ func NRune(s string, n int) rune {
 	return nrune[n-1]
 
 }
-
-	//len := 0
-	//for index := range runes {
-	//	len = index
-	//}
-	//
-	//if n-1 >= 0 && n-1 <= len {
-	//	return runes[n-1]
-	//}
-	//	return 0
-	//}
