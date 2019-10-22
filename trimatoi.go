6b15117912414056f3f0033b2bafbb5da0545bd6
dauren2089 <dauren.muk@gmail.com>
2019-10-22 16:59:24 +0600

    trimatoi has added

--- trimatoi.go
+++ trimatoi.go
@@ -3,16 +3,20 @@ package piscine
 func TrimAtoi(s string) int {
 	nbr := 0
 	sign := true
+	start := false
 
 	for _, value := range s {
 		if (value < '0') || (value > '9') {
-			if value == '-' {
-				sign = false
-			} else if value == '+' {
-				sign = true
+			if start == false {
+				if value == '-' {
+					sign = false
+				} else if value == '+' {
+					sign = true
+				}
 			}
 
 		} else {
+			start = true
 			for n := 0; n <= 9; n++ {
 				if rune(n+48) == value {
 					nbr = nbr*10 + n
@@ -22,7 +26,7 @@ func TrimAtoi(s string) int {
 		}
 	}
 	if sign == false {
-		nbr = nbr * -1
+		nbr *= -1
 	}
 	return nbr
 }
