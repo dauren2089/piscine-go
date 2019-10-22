b750b0750aa6f9caf00d2739d601a1762186bc26
dauren2089 <dauren.muk@gmail.com>
2019-10-22 16:07:48 +0600

    printnbrinorder has changed

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -8,8 +8,8 @@ import (
 func PrintNbrInOrder(n int) {
 	sentence := strconv.Itoa(n)
 
-	for i :=48; i <= 57; i++ {
-		//z01.PrintRune(rune(i))
+	for i := 48; i <= 57; i++ {
+
 		for _, value := range sentence {
 			if value == rune(i) {
 				z01.PrintRune(value)
