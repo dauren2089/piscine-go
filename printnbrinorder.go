978e8d7e136d0533353a431ada52359d724168fe
dauren2089 <dauren.muk@gmail.com>
2019-10-22 16:04:36 +0600

    printnbrinorder has changed

---
+++ printnbrinorder.go
@@ -0,0 +1,19 @@
+package piscine
+import (
+	"github.com/01-edu/z01"
+	"strconv"
+)
+
+func PrintNbrInOrder(n int) {
+	sentence := strconv.Itoa(n)
+
+	for i :=48; i<=57; i++ {
+		//z01.PrintRune(rune(i))
+		for _, value := range sentence {
+			if value == rune(i) {
+				z01.PrintRune(value)
+			}
+		}
+	}
+
+}
