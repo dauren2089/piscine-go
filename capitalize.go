ded3f51a6883676c423769ade4f8255a9883e8f5
dauren2089 <dauren.muk7@gmail.com>
2019-10-22 19:47:19 +0600

    capitalize has changed

--- capitalize.go
+++ capitalize.go
@@ -1,26 +1,44 @@
 package piscine
 
 func Capitalize(s string) string {
+
+	var checkbool bool
+
 	sentence := []rune(s)
 
-	for index, value := range sentence {
-		if value > 64 && value < 91 {
-			sentence[index] = value + 32
-		}
-	}
+	for i := 0; i < strLenght(s); i++ {
 
-	for index, value := range sentence {
-		if value > 96 && value < 123 {
-			if index == 0 {
-				sentence[index] = value - 32
-			} else if (sentence[index-1] < 'a' || sentence[index-1] > 'z') && (sentence[index-1] < 'A' || sentence[index-1] > 'Z') {
-				if sentence[index-1] < '0' || sentence[index-1] > '9' {
-					sentence[index] = value - 32
-				}
+		if isItWord(sentence[i]) == true && checkbool {
+
+			if sentence[i] >= 'a' && sentence[i] <= 'z' {
+				sentence[i] = 'A' - 'a' + sentence[i]
+
 			}
+			checkbool = false
+		} else if sentence[i] >= 'A' && sentence[i] <= 'Z' {
+			sentence[i] = 'a' - 'A' + sentence[i]
+		} else if isItWord(sentence[i]) == false {
+			checkbool = true
 		}
 	}
-
 	return string(sentence)
+}
 
+func strLenght(str string) int {
+
+	count := 0
+
+	for index := range str {
+		count = index + 1
+	}
+
+	return count
+}
+
+func isItWord(str rune) bool {
+
+	if (str >= 'A' && str <= 'Z') || (str >= 'a' && str <= 'z') || (str >= '0' && str <= '9') {
+		return true
+	}
+	return false
 }
