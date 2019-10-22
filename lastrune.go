211b5d7ab1a48b62bc63dda7bffd68f8ac3cb621
dauren2089 <dauren.muk7@gmail.com>
2019-10-22 18:54:57 +0600

    lastrune has changed

--- lastrune.go
+++ lastrune.go
@@ -7,18 +7,10 @@ func LastRune(s string) rune {
 	lastindex := 0
 
 	for index := range lastrune {
+
 		lastindex = index
 	}
 
 	return lastrune[lastindex]
 
 }
-
-//runes := []rune(s)
-//len := 0
-//for index := range runes {
-//len = index
-//}
-//
-//return runes[len]
-//}
