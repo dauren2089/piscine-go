b7547f13534b9a6872c3a22f01a50794d8774dce
dauren2089 <dauren.muk@gmail.com>
2019-10-22 16:19:52 +0600

    printnbrinorder has changed

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,58 +1,38 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
-func PrintNbrInOrder(n int64) {
-	if n < 0 {
+func sortInt(arr []int) []int {
+	for run := true; run; {
+		run = false
+		for i := 1; i < len(arr); i++ {
+			if arr[i] < arr[i-1] {
+				arr[i], arr[i-1] = arr[i-1], arr[i]
+				run = true
+			}
+		}
+	}
+	return arr
+}
+
+func intToDigits(n int) (digits []int) {
+	for n > 0 {
+		if n == 0 {
+			digits = append(digits, 0)
+		} else {
+			digits = append(digits, n%10)
+		}
+		n /= 10
+	}
+	return
+}
+
+func PrintNbrInOrder(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
 		return
 	}
-	if n == 0 {
-
-		z01.PrintRune('0')
-
-	}
-
-	var sentense []int64
-
-	sentense = IntToSlice(n, sentense)
-
-	SortInteger(sentense)
-
-	for i := range sentense {
-
-		for j := '0'; j <= '9'; j++ {
-
-			if rune(sentense[i]+48) == j {
-
-				z01.PrintRune(j)
-
-			}
-
-		}
-	}
-}
-
-func IntToSlice(n int64, sentense []int64) []int64 {
-	if n != 0 {
-		i := n % 10
-		// sequence = append(sequence, i) // reverse order output
-		sentense = append([]int64{i}, sentense...)
-		return IntToSlice(n/10, sentense)
-	}
-	return sentense
-}
-
-func SortInteger(str []int64) {
-	L := len(str)
-
-	for i := 0; i < L; i++ {
-
-		for j := 0; j < (L - 1 - i); j++ {
-			if str[j] > str[j+1] {
-				str[j], str[j+1] = str[j+1], str[j]
-			}
-		}
+	for _, c := range sortInt(intToDigits(n)) {
+		z01.PrintRune(rune(c) + '0')
 	}
 }
