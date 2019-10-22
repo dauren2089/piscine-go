a75feb20ad4677c1f5397775477d85b22019d2c6
dauren2089 <dauren.muk7@gmail.com>
2019-10-22 19:04:26 +0600

    index has changed

--- index.go
+++ index.go
@@ -1,37 +1,32 @@
 package piscine
 
 func Index(s string, toFind string) int {
+
+	sentence := StrLen(s)
+	letter := StrLen(toFind)
+
 	if toFind == "" {
 		return 0
 	}
-	sentence := []rune(s)
-	find := []rune(toFind)
-	len := 0
-	for index := range find {
-		len = index
-	}
-	str1 := ""
-	str2 := ""
-	for index := range sentence {
-		str1 = string(sentence[index : index+len+1])
-		str2 = string(find)
-		if str1 == str2 {
-			return index
-		}
 
+	for i := 0; i <= sentence-letter; i++ {
+
+		if toFind == s[i:i+letter] {
+
+			return (i)
+		}
 	}
 	return -1
 
 }
 
-//j:=[]rune(s)
-//l:=[]rune(toFind)
-//n:=lent(j)
-//k:=lent(l)
-//
-//for i:=0; i<= n-k; i++ {
-//if toFind==s[i:i+k]{
-//return (i)
-//}
-//}
-//return -1
+func StrLen(str string) int {
+
+	count := 0
+
+	for index := range str {
+		count = index + 1
+	}
+
+	return count
+}
