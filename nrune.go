67d6fddc333ecb356abb91d84fe859546b42fe9b
dauren2089 <dauren.muk7@gmail.com>
2019-10-22 18:51:27 +0600

    nrune has changed

--- nrune.go
+++ nrune.go
@@ -3,7 +3,17 @@ package piscine
 func NRune(s string, n int) rune {
 
 	nrune := []rune(s)
+	count := 0
 
-	return nrune[n-1]
+	for value := range nrune {
+		count = value
+	}
+
+	if n-1 >= 0 && n-1 <= count {
+
+		return nrune[n-1]
+
+	}
+	return 0
 
 }
