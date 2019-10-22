c399e91fbe15670a03a03c7dcc5af5cd79ad63cf
dauren2089 <dauren.muk@gmail.com>
2019-10-22 15:59:34 +0600

    alphacount has changed

--- alphacount.go
+++ alphacount.go
@@ -4,19 +4,23 @@ package piscine
 
 func AlphaCount(str string) int {
 	count := 0
-	for i:= 'a'; i<='z'; i++ {
+
+	for i:= 'a'; i <= 'z'; i++ {
 		for _, value := range str {
 			if value == i {
 				count++
 			}
 		}
 	}
-	for j:= 'A'; j<='Z'; j++{
+
+	for j:= 'A'; j <= 'Z'; j++ {
+
 		for _, value := range str {
 			if value == j {
 				count++
 			}
 		}
+
 	}
 	//fmt.Println(str)
 	return count
