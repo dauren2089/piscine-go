ab52f32da6fb3b30016211465c54c5fe7afdbfeb
dauren2089 <dauren.muk@gmail.com>
2019-10-22 18:07:08 +0600

    concat has added

---
+++ concat.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Concat(str1 string, str2 string) string {
+	return str1 + str2
+
+}
+
+//func Concat(str1 string, str2 string) string {
+//
+//	result:=str1+str2
+//	return result
+//}
