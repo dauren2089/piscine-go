d0926fbb93c07ca14c462b6c9444b72d449169e1
dauren2089 <dauren.muk@gmail.com>
2019-10-22 11:20:18 +0600

    alphacount has added

---
+++ alphacount.go
@@ -0,0 +1,23 @@
+package piscine
+
+//import "fmt"
+
+func AlphaCount(str string) int {
+	count := 0
+	for i:= 'a'; i<='z'; i++ {
+		for _, value := range str {
+			if value == i {
+				count++
+			}
+		}
+	}
+	for j:= 'A'; j<='Z'; j++{
+		for _, value := range str {
+			if value == j {
+				count++
+			}
+		}
+	}
+	//fmt.Println(str)
+	return count
+}
