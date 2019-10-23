94ad2f475b7fe8a54dde84fe6a0422b3afd0691b
Dauren <37897996+dauren2089@users.noreply.github.com>
2019-10-23 14:48:28 +0600

    Update islower.go

--- islower.go
+++ islower.go
@@ -15,15 +15,3 @@ func IsLower(str string) bool {
 	}
 	return true
 }
-
-//func IsLower(str string) bool {
-//	h := []rune(str)
-//
-//	for i := 0; i <= lent(h)-1; i++ {
-//		if (h[i] >= 0) && (h[i] <= 96) || (h[i] >= 123) && (h[i] <= 127){
-//			return false
-//		}
-//
-//	}
-//	return true
-//}
