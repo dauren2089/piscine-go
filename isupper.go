a120e957c549d051411c6f4106e71ab37a1cc43e
dauren2089 <dauren.muk@gmail.com>
2019-10-23 10:33:15 +0600

    commit

--- isupper.go
+++ isupper.go
@@ -13,17 +13,3 @@ func IsUpper(str string) bool {
 	}
 	return true
 }
-
-
-//func IsUpper(str string) bool {
-//	h := []rune(str)
-//
-//	for i := 0; i <= lent(h)-1; i++ {
-//		if (h[i] >= 0) && (h[i] <= 64) || (h[i] >= 91) && (h[i] <= 127){
-//			return false
-//		}
-//
-//	}
-//	return true
-//}
-
