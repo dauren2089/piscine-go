b6696d1f0a5abc36b7858e05aaf61a76bc92b300
dauren2089 <dauren.muk7@gmail.com>
2019-10-23 04:19:46 -0700

    sort committed

--- sortparams/main.go
+++ sortparams/main.go
@@ -9,36 +9,22 @@ import (
 func main() {
 
 	arguments := os.Args
-	asciitable := string("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 
-	//for i := 32; i <= 125; i++ {
-	//z01.PrintRune(rune(i))
-	//z01.PrintRune(10)
+	lenght := strLenght(arguments) - 1
 
-	for _, value := range asciitable {
-		strvalue := string(value)
-		for j := 1; j < strLenght(arguments); j++ {
-			//printRune(arguments[j])
-			//z01.PrintRune(10)
+	arguments = arguments[1 : lenght+1]
 
-			if strvalue == arguments[j] {
-				printRune(arguments[j])
-				z01.PrintRune(10)
-			}
+	sortedArguments := sortNumbers(arguments, lenght)
+
+	for _, value := range sortedArguments {
+
+		for _, runes := range value {
+			z01.PrintRune(runes)
 		}
+
+		z01.PrintRune(10)
 	}
 
-}
-
-func printRune(str string) {
-
-	runes := []rune(str)
-
-	for _, value := range runes {
-
-		z01.PrintRune(value)
-
-	}
 }
 
 func strLenght(str []string) int {
@@ -50,4 +36,15 @@ func strLenght(str []string) int {
 	}
 
 	return count
+}
+
+func sortNumbers(result []string, lenght int) []string {
+	for i := 0; i < lenght; i++ {
+		for j := i + 1; j < lenght; j++ {
+			if result[i] > result[j] {
+				result[i], result[j] = result[j], result[i]
+			}
+		}
+	}
+	return result
 }
