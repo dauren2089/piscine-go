c7253fb267a88724ca1e20a90b28eb2f54d86459
dauren2089 <dauren.muk7@gmail.com>
2019-10-23 03:37:17 -0700

    committed

--- sortparams/main.go
+++ sortparams/main.go
@@ -9,15 +9,19 @@ import (
 func main() {
 
 	arguments := os.Args
+	asciitable := string("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 
-	for i := 32; i <= 125; i++ {
-		//z01.PrintRune(rune(i))
-		//z01.PrintRune(10)
+	//for i := 32; i <= 125; i++ {
+	//z01.PrintRune(rune(i))
+	//z01.PrintRune(10)
 
+	for _, value := range asciitable {
+		strvalue := string(value)
 		for j := 1; j < strLenght(arguments); j++ {
 			//printRune(arguments[j])
 			//z01.PrintRune(10)
-			if string(i) == arguments[j] {
+
+			if strvalue == arguments[j] {
 				printRune(arguments[j])
 				z01.PrintRune(10)
 			}
@@ -28,9 +32,9 @@ func main() {
 
 func printRune(str string) {
 
-	Runes := []rune(str)
+	runes := []rune(str)
 
-	for _, value := range Runes {
+	for _, value := range runes {
 
 		z01.PrintRune(value)
 
