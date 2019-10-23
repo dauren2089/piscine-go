f55a6dbfa50bba6d2e49f48d40c7f22d4d57aaef
dauren2089 <dauren.muk@gmail.com>
2019-10-23 10:35:38 +0600

    commit

--- isprintable.go
+++ isprintable.go
@@ -2,25 +2,13 @@ package piscine
 
 func IsPrintable(str string) bool {
 	sentence := []rune(str)
-	for index, value := range sentence {
-		if !(value >= ' ') {
-			index = index + 0
 
+	for i := 0; i <= strLen(str)-1; i++ {
+		if (sentence[i] >= 0) && (sentence[i] <= 31) {
 			return false
 		}
+
 	}
 	return true
+
 }
-
-
-//func IsPrintable(str string) bool {
-//	h := []rune(str)
-//
-//	for i := 0; i <= len(h)-1; i++ {
-//		if (h[i] >= 0) && (h[i] <= 31) {
-//			return false
-//		}
-//
-//	}
-//	return true
-//}
