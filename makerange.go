ec2516df3bf87ced9d28eef7a1394ae778a2d85b
dauren2089 <dauren.muk7@gmail.com>
2019-10-24 01:31:12 -0700

    makerange has changed

--- makerange.go
+++ makerange.go
@@ -1,18 +1,14 @@
 package piscine
 
-import "fmt"
-
 func MakeRange(min, max int) []int {
 
 	var size int
 
 	if min < max {
 		size = max - min
-		fmt.Println("size: ", size)
 
 	} else {
 		size = 0
-		fmt.Println("size: ", size)
 	}
 
 	rangeNbr := make([]int, size)
